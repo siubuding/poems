54771c5801659eeb6ff7f9db134497525255a353
Z <zijianhuang2000@gmail.com>
2022-08-18 16:32:33 +1000

    kai

---
+++ docs/kai/conf/siteconfig.js
@@ -0,0 +1,6 @@
+const SITE_CONFIG = {
+	siteName: '少不丁诗集',
+	staticReadonly: true,
+	hasSecondary: true,
+	isSecondary: false
+}
---
+++ docs/kai/conf/siteconfig2nd.js
@@ -0,0 +1,6 @@
+const SITE_CONFIG = {
+	siteName: '少不丁詩集',
+	staticReadonly: true,
+	hasSecondary: true,
+	isSecondary: true
+}
