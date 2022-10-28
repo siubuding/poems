d0d2bb3ca5a66bd755182a68c9ab5e947378752d
Z <zijianhuang2000@gmail.com>
2022-10-28 14:21:02 +1000

    ok

--- docs/conf/siteconfig.js
+++ docs/conf/siteconfig.js
@@ -1,6 +1,8 @@
 const SITE_CONFIG = {
 	siteName: '少不丁诗集',
 	staticReadonly: true,
+	copyPoemEnabled: true,
+	copyAlbumEnabled: true,
 	staticDatas: [
 		{filename: 'primary', secondaryFilename: 'secondary'}
 	]
